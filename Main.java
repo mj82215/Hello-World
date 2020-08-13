b51b87c74b3ea55fc8379f188e0ce24c4c149f96
mj82215 <69613742+mj82215@users.noreply.github.com>
2020-08-12 19:59:45 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("My name is pronounced 'Muh-roon-my-ee', but I go by my initals MJ");
+		System.out.println("One of my hobbies is making and collecting origami");
+		System.out.println("My favorite TV show is called 'Major Crimes', and it is a crime show");
+		System.out.println("My music taste is best described as alt-rock or pop-punk");
+		System.out.println("I want to go to college at UC Irvine, UC Berkeley, or UC San Diego, but any UC would be nice");
+	}
+}
